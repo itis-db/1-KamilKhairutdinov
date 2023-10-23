2ca7ffc6523072948f3ee5105a8bcee483a154bb
1k_mile <kamafeed1212@gmail.com>
2023-10-23 19:58:48 +0300

    add lvl1

--- activity.sql
+++ activity.sql
@@ -1,1 +1,26 @@
----
+with recursive activity_hierarchy as (
+    select
+        act.activityid as id,
+        act.parentid,
+        act.activitytypeid,
+        act.code as activityid,
+        at.name as activity_name,
+        cast(at.name as VARCHAR) as hierarchy_path
+    from activity as act
+    join activitytype as at on at.activitytypeid = act.activitytypeid
+    where act.parentid is null
+
+    union
+
+    select
+        act.activityid as id,
+        act.parentid,
+        act.activitytypeid,
+        act.code as activityid,
+        at.name as activity_name,
+        concat(h.hierarchy_path, ' -> ', at.name) as hierarchy_path
+    from activity as act
+    join activitytype as at on at.activitytypeid = act.activitytypeid
+    join activity_hierarchy as h on act.parentid = h.id
+)
+select * from activity_hierarchy;
